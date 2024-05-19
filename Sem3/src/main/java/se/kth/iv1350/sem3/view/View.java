01f87d941d7f0b2532b9a1dbc75be1559ba6d9e8
Teoman Köylüoglu <Teoman.sevh@outlook.com>
2024-05-19 19:24:52 +0200

    Update View.java

--- Sem3/src/main/java/se/kth/iv1350/sem3/view/View.java
+++ Sem3/src/main/java/se/kth/iv1350/sem3/view/View.java
@@ -123,7 +123,7 @@ public class View {
             errorMsgHandler.showMessage("The payment method you have entered is invalid, we only accept cash or credit card. Please try a different payment method.");
         }
     }
-    
+
     private void printReturnChangeToCustomer(double change) {
         DecimalFormat df = new DecimalFormat("#.##");
         System.out.println("\nChange to give back to Customer: " + df.format(change));
