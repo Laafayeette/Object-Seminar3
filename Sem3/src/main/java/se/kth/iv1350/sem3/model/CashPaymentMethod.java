92084d84ae3810840d57f43f008fbe52ffd15177
Teoman Köylüoglu <Teoman.sevh@outlook.com>
2024-05-22 16:44:33 +0200

    Update CashPaymentMethod.java

--- Sem3/src/main/java/se/kth/iv1350/sem3/model/CashPaymentMethod.java
+++ Sem3/src/main/java/se/kth/iv1350/sem3/model/CashPaymentMethod.java
@@ -7,7 +7,7 @@ public class CashPaymentMethod implements PaymentMethodStrategy {
 
     /**
      * An implementation of the {@link PaymentMethodStrategy} interface.
-     * Implements the <code>calculateCustomerChange</code> interface.
+     * Implements the <code>calculateCustomerChange</code> interface method.
      * Calculates the amount of change to be given back to the customer.
      * @param amount The amount given from the customer to pay.
      * @param currentTotalPrice The total price generated from the sale.
