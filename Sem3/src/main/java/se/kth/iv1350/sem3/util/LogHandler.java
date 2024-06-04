8e550e032c01e02488cd3fe49e54863cf4204cf7
Teoman Köylüoglu <Teoman.sevh@outlook.com>
2024-06-04 02:01:05 +0200

    Added constructor overloading for LogHandler
    This is to handle the additional case wherein we specify the file name directly when calling upon the constructor.
    
    Co-Authored-By: ermia1230 <149333804+ermia1230@users.noreply.github.com>

--- Sem3/src/main/java/se/kth/iv1350/sem3/util/LogHandler.java
+++ Sem3/src/main/java/se/kth/iv1350/sem3/util/LogHandler.java
@@ -16,6 +16,14 @@ public class LogHandler {
         }
     }
 
+    public LogHandler(String fileName) {
+        try {
+            logStream = new PrintWriter(new FileWriter(fileName, true), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void log(Exception e) {
         logStream.println("An exception was thrown at: " + localDateTime + " with the following message: \n" + e.getMessage());
         System.out.println("\n\n");
