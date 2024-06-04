79a6f871ab7006d5306383db7f37727ac9fc4bc5
Teoman Köylüoglu <Teoman.sevh@outlook.com>
2024-06-04 22:33:19 +0200

    Update TotalRevenueView.java
    Co-Authored-By: ermia1230 <149333804+ermia1230@users.noreply.github.com>

--- Sem3/src/main/java/se/kth/iv1350/sem3/view/TotalRevenueView.java
+++ Sem3/src/main/java/se/kth/iv1350/sem3/view/TotalRevenueView.java
@@ -1,8 +1,5 @@
 package se.kth.iv1350.sem3.view;
 
-import se.kth.iv1350.sem3.model.Sale;
-import se.kth.iv1350.sem3.model.SaleObserver;
-
 /**
  * This class represents and handles the <code>totalRevenue</code> to be printed on the user interface.
  */
